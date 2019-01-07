6bade89c386cea04c92bf9af658131392c9e4505
jsphLim <jsphlim@stu2016.jnu.edu.cn>
2019-01-07 19:34:14 +0800

    Update main.go

--- aho-Corasick/main.go
+++ aho-Corasick/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	content := "wm.Lan is the excellent professor of JNU"
+	content := "Prof. wm Lan is the excellent professor of JNU"
 	aho := ac.CreateAhoCorasick()
 	aho.Add("wm")
 	aho.Add("is")
@@ -15,7 +15,7 @@ func main() {
 	aho.Add("JNU")
 	aho.BuildAhoCorasick()
 	results := aho.ScanAhoCorasick(content)
-	fmt.Println("匹配成功的词: ")
+	fmt.Println("match words are:")
 	for _, result := range results {
 		//fmt.Println(result)
 		fmt.Println(string([]rune(content)[result.Begin : result.End+1]))
